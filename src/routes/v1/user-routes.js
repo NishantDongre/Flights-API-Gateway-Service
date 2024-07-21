e3ef9259cdfd131a3a3804fc0666438bf4453823
Nishant <nishantdongre30@gmail.com>
2024-07-21 14:06:30 +0530

    FEAT: Added Users Model and new user signup api

---
+++ src/routes/v1/user-routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { UserController } = require("../../controllers");
+const { AuthRequestMiddlewares } = require("../../middlewares");
+const router = express.Router();
+
+router.post(
+    "/",
+    AuthRequestMiddlewares.validateAuthRequest,
+    UserController.signup
+);
+
+module.exports = router;
