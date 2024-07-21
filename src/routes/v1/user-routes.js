6662e691c0568bc30848b44f97af7a00afa51792
Nishant <nishantdongre30@gmail.com>
2024-07-21 14:53:53 +0530

    FEAT: Added signin with jwt creation

--- src/routes/v1/user-routes.js
+++ src/routes/v1/user-routes.js
@@ -4,9 +4,14 @@ const { AuthRequestMiddlewares } = require("../../middlewares");
 const router = express.Router();
 
 router.post(
-    "/",
+    "/signup",
     AuthRequestMiddlewares.validateAuthRequest,
     UserController.signup
+);
+router.post(
+    "/signin",
+    AuthRequestMiddlewares.validateAuthRequest,
+    UserController.signin
 );
 
 module.exports = router;
